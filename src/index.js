07bdd71520cb13442f374b619e09bd5908d055ed
Andrey Borodenkov <borodenkov.andrey@gmail.com>
2020-10-30 19:54:17 +0200

    input

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './styles.css';
 import './scss/base.scss'
+import './js/search-section'
---
+++ src/js/search-section.js
@@ -0,0 +1,36 @@
+const refs = {
+  form: document.querySelector('.search-form'),
+  inputFilms: document.querySelector('.input'),
+  films: document.querySelector('.films'),
+};
+
+let inputValue = null;
+
+// faafef79ea4c171e4623351b880cc7ac
+function fetchFilms(inputValue) {
+  return fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=faafef79ea4c171e4623351b880cc7ac&query=${inputValue}`,
+  )
+    .then(response => response.json())
+    .catch(error => console.error(error));
+}
+
+const searchFilms = event => {
+  event.preventDefault();
+
+  refs.films.innerHTML = ' ';
+  inputValue = refs.inputFilms.value;
+  fetchFilms(inputValue).then(data => {
+    if (data.total_results !== 0) {
+      console.log(data.results);
+    } else {
+        console.log('нет такого фильма')
+    }
+  });
+  // В дальнейшем console.log(data.results) заменяется на createCardFunc
+  refs.form.reset();
+};
+
+refs.form.addEventListener('submit', searchFilms);
+
+
