f2272994acee069075e9df0dd8d0847aaaf1f4a9
Malyshko Serhii <malyshkosergiy@gmail.com>
2020-11-01 14:05:41 +0200

    search request and render of the page have been separated

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './styles.css';
 import './scss/base.scss';
 import './scss/components/next-prev_btn.scss';
 // import './js/next-prev_btn';
-import './js/search-section';
+import './js/searchAndPlaginationHomePage';
 // import './js/filmDetailPage';
