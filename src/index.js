5df613e5454d5a39f33f880c61830064deec0da7
zaharius <zaharius.ltd@gmail.com>
2020-11-01 17:50:01 +0200

    chenge code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@
 import './styles.css';
 import './scss/base.scss';
 import './scss/components/next-prev_btn.scss';
-// import './js/next-prev_btn';
+import './js/next-prev_btn';
 import './js/search-section';
 // import './js/filmDetailPage';
